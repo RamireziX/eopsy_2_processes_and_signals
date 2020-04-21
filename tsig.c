47a9a8fdc0eef5b7d315a099ca475a964cda148d
Alex <alexander.wrzosek@o2.pl>
2020-04-21 16:58:00 +0200

    child processes added

--- tsig.c
+++ tsig.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <sys/wait.h>
+
+void childProcess(int howMany){
+    pid_t forkResult;
+    for(int i=0;i<howMany;i++)
+    { 
+        forkResult = fork();
+        if(forkResult == 0) 
+        { 
+            printf("child[%d]: identifier of the parent process: %d\n", getpid(), getppid()); 
+            sleep(1);
+            printf("child[%d]: execution complete\n", getpid()); 
+            exit(0); 
+        }
+        sleep(1);
+    } 
+    for(int i=0;i<howMany;i++){
+    wait(NULL); 
+    }
+}
 int main() {
-   printf("Hello, World!");
+   
+    int NUM_CHILD = 2;
+    
+    childProcess(NUM_CHILD);
+    
+    printf("parent[%d]: child processes created successfully\n", getppid());
+    
    return 0;
 }
+
