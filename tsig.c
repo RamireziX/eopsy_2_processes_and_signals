63e9742db3d341d9a08cfc2dce8b1ca65f6c6cb9
Alex <alexander.wrzosek@o2.pl>
2020-04-22 17:28:36 +0200

    2 finished

--- tsig.c
+++ tsig.c
@@ -3,6 +3,7 @@
 #include <sys/types.h> 
 #include <unistd.h> 
 #include <sys/wait.h>
+#define WITH_SIGNALS
 
 void childProcess(int howMany){
     pid_t forkResult;
@@ -16,20 +17,36 @@ void childProcess(int howMany){
             printf("child[%d]: execution complete\n", getpid()); 
             exit(0); 
         }
+        else if(forkResult < 0){
+            fprintf(stderr, "child[%d]: error\n", getpid()); 
+        }
         sleep(1);
     } 
+    
+    printf("parent[%d]: child processes created successfully\n", getpid());
+    
     for(int i=0;i<howMany;i++){
-    wait(NULL); 
+        wait(NULL); 
     }
+    printf("parent[%d]: no more child processes\n", getpid());
+   
 }
+
 int main() {
    
-    int NUM_CHILD = 2;
+    int NUM_CHILD = 3;
     
-    childProcess(NUM_CHILD);
+    #ifdef WITH_SIGNALS
+        return 0;
+    #endif
+    #ifndef WITH_SIGNALS
+        childProcess(NUM_CHILD);
+        return 0;
+    #endif
     
-    printf("parent[%d]: child processes created successfully\n", getppid());
+    return 0;
     
-   return 0;
+    
+   
 }
 
