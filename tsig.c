0c7cd6776451c0a365eca012ca747515a9890768
Alex <alexander.wrzosek@o2.pl>
2020-04-23 19:23:35 +0200

    final commit

--- tsig.c
+++ tsig.c
@@ -3,50 +3,90 @@
 #include <sys/types.h> 
 #include <unistd.h> 
 #include <sys/wait.h>
+#include <signal.h>
+#include <stdbool.h>
+
 #define WITH_SIGNALS
 
-void childProcess(int howMany){
+#ifdef WITH_SIGNALS
+//global variable for my handler
+bool mark = false;
+
+//own SIGINT handlers
+void sigIntHandler(int sig_num){
+    signal(SIGINT, sigIntHandler); 
+    printf("parent[%d]: received interrupt, shutting down child processes\n", getpid()); 
+    mark = true;
+}
+
+void sigIntHandlerChild(int sig_num){
+    signal(SIGINT, sigIntHandler); 
+    printf("child[%d]: interrupted, terminating execution\n", getpid()); 
+}
+#endif
+
+//parent process
+void childProcesses(int howMany){
     pid_t forkResult;
+    //ignore all signals
+    #ifdef WITH_SIGNALS
+    signal(SIGINT, SIG_IGN);
+    //except for SIGCHLD
+    signal(SIGCHLD, SIG_DFL);
+    //own SIGINT handler
+    signal(SIGINT, sigIntHandler); 
+    #endif
+    // create NUM_CHILD children
     for(int i=0;i<howMany;i++)
-    { 
+    {   
         forkResult = fork();
+        // child process
         if(forkResult == 0) 
         { 
+            #ifdef WITH_SIGNALS
+            //if mark is set, kill all child processes
+            if(mark == true){
+                printf("parent[%d]: creation of child[%d] interrupted\n", getppid(), getpid()); 
+                kill(getpid(), SIGTERM);
+            }
+            //ignore all signals
+            signal(SIGINT, SIG_IGN);
+            //own SIGINT handler
+            signal(SIGINT, sigIntHandlerChild); 
+            #endif
             printf("child[%d]: identifier of the parent process: %d\n", getpid(), getppid()); 
-            sleep(1);
+            //sleep 10 seconds (execution)
+            sleep(10);
             printf("child[%d]: execution complete\n", getpid()); 
             exit(0); 
         }
+        //error when creating a child
         else if(forkResult < 0){
             fprintf(stderr, "child[%d]: error\n", getpid()); 
         }
+        //sleep 1 second between creating children
         sleep(1);
-    } 
-    
+    }
+    #ifdef WITH_SIGNALS
+    if(mark != true)
+    #endif
     printf("parent[%d]: child processes created successfully\n", getpid());
-    
     for(int i=0;i<howMany;i++){
+        // wait for child processes to end
         wait(NULL); 
     }
     printf("parent[%d]: no more child processes\n", getpid());
-   
+    //restore old handlers
+    #ifdef WITH_SIGNALS
+    signal(SIGINT, SIG_DFL);
+    #endif
 }
 
 int main() {
    
-    int NUM_CHILD = 3;
-    
-    #ifdef WITH_SIGNALS
-        return 0;
-    #endif
-    #ifndef WITH_SIGNALS
-        childProcess(NUM_CHILD);
-        return 0;
-    #endif
-    
+    int NUM_CHILD = 5;
+    childProcesses(NUM_CHILD);
+
     return 0;
-    
-    
-   
 }
 
